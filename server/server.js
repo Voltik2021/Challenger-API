7918df87dbf13c6ae3df9f7760c2beb393a76138
Voltik <Voltik2019@yandex.ru>
2021-06-10 11:20:46 +0300

    prepare heroku

--- server/server.js
+++ server/server.js
@@ -270,4 +270,4 @@ app.get('/getUserCompletedMyTest', (req, res) => {
 })
 
 
-app.listen(3000, () => { console.log('Example app listening on port 3000!') });
+app.listen(process.env.PORT || 3000, () => { console.log('Example app listening on port 3000!') });
